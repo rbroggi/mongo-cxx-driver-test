579fecda016412ad1f457e40af0cb27467d99e7e
rbroggi <rbroggi12@gmail.com>
2018-11-07 13:40:54 +0100

    correction and enhancement with output

--- mongo_test.cpp
+++ mongo_test.cpp
@@ -1,24 +1,104 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <thread>
 
 #include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/document/view.hpp>
+#include <bsoncxx/builder/basic/kvp.hpp>
 #include <bsoncxx/json.hpp>
 
 #include <mongocxx/client.hpp>
+#include <mongocxx/pool.hpp>
 #include <mongocxx/instance.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::stream::document;
+using bsoncxx::document::view;
+using bsoncxx::builder::stream::finalize;
+
+typedef std::string db_name;
+typedef std::string collection_name;
 
 int main(int, char**) {
     mongocxx::instance inst{};
-    mongocxx::client conn{mongocxx::uri{}};
 
-    bsoncxx::builder::stream::document document{};
+    //Create connection pool
+    mongocxx::pool pool{mongocxx::uri{}};
 
-    auto collection = conn["testdb"]["testcollection"];
-    document << "hello" << "world";
+    //Define generator lambda generator - this lambda insert documents into the db and colection specified
+    auto generator = [&pool](const db_name db, const collection_name col, view view) {
+        auto client = pool.acquire();
+        auto collection = (*client)[db][col];
+        collection.insert_one(view);
+        std::cout << "Inserted into " << db << ":" << col << std::endl;
+    };
 
-    collection.insert_one(document.view());
-    auto cursor = collection.find({});
+    //Define lambda reader method
+    auto retrieve = [&pool](const db_name db, const collection_name col, const std::int64_t id, std::vector<view> & doc_views) {
+        auto client = pool.acquire();
+        auto collection = (*client)[db][col];
+        for (auto&& doc : collection.find(document{} << "id" << id << finalize) ) {
+            doc_views.push_back(doc);
+        }
+    };
 
-    for (auto&& doc : cursor) {
-        std::cout << bsoncxx::to_json(doc) << std::endl;
+    //Define lambda delete method
+    auto remove = [&pool](const db_name db, const collection_name col, const std::int64_t id) {
+        auto client = pool.acquire();
+        auto collection = (*client)[db][col];
+        collection.delete_many(document{} << "id" << id << finalize);
+    };
+
+    //Using variables to simulate procedure
+    std::vector<int> ids = {1, 2, 3, 4, 5, 6};
+    std::vector<std::string> names  = {"a", "b", "c", "d", "e", "f"};
+    std::unordered_map<int, db_name> ids_to_db = {
+            {1, "dbOne"},
+            {2, "dbTwo"},
+            {3, "dbOne"},
+            {4, "dbTwo"},
+            {5, "dbOne"},
+            {6, "dbTwo"},
+    };
+
+    std::unordered_map<int, collection_name> ids_to_collections = {
+            {1, "collectionOne"},
+            {2, "collectionTwo"},
+            {3, "collectionThree"},
+            {4, "collectionOne"},
+            {5, "collectionTwo"},
+            {6, "collectionThree"},
+    };
+
+    //Delete all enties before begining
+    for (auto i : ids) {
+        remove(ids_to_db[i], ids_to_collections[i], i);
+    }
+
+
+    //Generation loop using separate threads
+    std::vector<std::shared_ptr<std::thread>> threads{};
+    for (auto i : ids) {
+        document doc{};
+        doc << "id" << i
+            << "name" << names[i-1];
+        std::shared_ptr<std::thread> runner =
+                std::make_shared<std::thread>(generator, ids_to_db[i], ids_to_collections[i], doc.view());
+        threads.push_back(runner);
+    }
+
+    std::cout << "Waiting for threads to finish..." << std::endl;
+    //wait for threads to finish
+    for (auto t : threads) (*t).join();
+
+    std::cout << "Threads finished..." << std::endl;
+
+    //Retriving using single thread
+    for (auto i : ids) {
+        std::vector<view> doc_views{};
+        retrieve(ids_to_db[i], ids_to_collections[i], i, doc_views);
+        for (auto view : doc_views) std::cout << bsoncxx::to_json(view) << std::endl;
     }
 }
